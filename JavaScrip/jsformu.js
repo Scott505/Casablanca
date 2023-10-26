8c0a7e934e4a2b82f1e6bf7d17ca11c706239398
Scott505 <142347083+Scott505@users.noreply.github.com>
2023-10-26 12:56:51 -0300

    Avanze form

--- JavaScrip/jsformu.js
+++ JavaScrip/jsformu.js
@@ -7,16 +7,20 @@ const form = document.getElementById('MyForm');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    let errores = 0;
+
     if (nombre.value.trim() === '') {
         nombre.classList.add('error');
-        nombre.placeholder = 'Ingrese'
+        nombre.placeholder = '*Debe ingresar un nombre*';
+        errores += 1;
     } else {
         nombre.classList.remove('error');
     }
 
     if (celular.value.trim() === '') {
         celular.classList.add('error');
-        celular.placeholder = '*Debe ingresar un nombre*';
+        celular.placeholder = '*Debe ingresar un celular*';
+        errores += 1;
     } else {
         celular.classList.remove('error');
     }
@@ -24,6 +28,7 @@ form.addEventListener('submit', (e) => {
     if (email.value.trim() === '' || !/^\S+@\S+\.\S+$/.test(email.textContent)) {
         email.classList.add('error');
         email.placeholder = '*Debe ingresar un email*';
+        errores += 1;
     } else {
         email.classList.remove('error');
     }
@@ -31,8 +36,20 @@ form.addEventListener('submit', (e) => {
     if (comentario.value.trim() === '') {
         comentario.classList.add('error');
         comentario.placeholder = '*Debe ingresar un nombre*';
+        errores += 1;
     } else {
         comentario.classList.remove('error');
     }
 
+    if (errores !== 0) {
+        envioError();
+    }else {
+
+    }
+
+    function envioError() {
+        formCont.innerHTML = '';
+        formCont.appendChild("*Error");
+    }
+    
 });
